dd5c99bcbbae1c461f97d09a8769ffb2418de186
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-07-06 18:11:00 +0530

    server2

--- auth/urls.py
+++ auth/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Django is working 🎉"})
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
      path('api/', include('authapp.urls')),
+     path('', home),
 ]
 
